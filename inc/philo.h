0f444250c74fc579c58974dc26fcfc67c65ad07d
Manuel Ignacio Luis Fuentes <mluis-fu@c2r14s2.42madrid.com>
2023-03-22 12:55:07 +0100

    added define on .h

--- inc/philo.h
+++ inc/philo.h
@@ -6,7 +6,7 @@
 /*   By: mluis-fu <mluis-fu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/22 12:01:04 by mluis-fu          #+#    #+#             */
-/*   Updated: 2023/03/22 12:39:54 by mluis-fu         ###   ########.fr       */
+/*   Updated: 2023/03/22 12:54:27 by mluis-fu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,17 @@
 
 # include "libft.h"
 # include <pthread.h>
+
+# define ATC_TAKE "has taken a fork"
+# define ATC_EAT "is eating"
+# define ATC_SLEEP "is sleeping"
+# define ATC_DIE "died"
+
+enum e_pstate
+{
+	P_IS_ALIVE = 1,
+	P_IS_DEAD = 0,
+};
 
 typedef struct srules
 {
@@ -27,8 +38,8 @@ typedef struct srules
 typedef struct s_philo
 {
 	int		*philo_id;
-	int		*left_m;
-	int		*right_m;
+	int		left_m;
+	int		right_m;
 	t_rules	rules;
 }			t_philo;
 
